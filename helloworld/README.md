f85d44d1077a81cdb7405cd0e4749ed7e015653f
yellagowthami01 <yella.gowthami01@gmail.com>
2017-01-24 19:29:12 -0600

    sample helloworld in angular js
    Helloworld project in angular js adding unit testing

---
+++ helloworld/README.md
@@ -0,0 +1,13 @@
+Setup
+
+npm use 5.0
+
+npm install
+
+node server.js
+
+Test
+
+npm install -g karma-cli
+
+karma start
---
+++ helloworld/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      './app/test/users.spec.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
